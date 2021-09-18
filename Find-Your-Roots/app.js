03c0d22eb61bbbd8c0d8180fa8e94a745f9925be
kaojonathan <kaojonathan1@gmail.com>
2021-09-18 11:56:53 -0400

    figured out the routes maybe?

--- Find-Your-Roots/app.js
+++ Find-Your-Roots/app.js
@@ -1,19 +1,23 @@
 const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
+require('dotenv/config');
 
 // Middlewares  - functions that run when we hit a route
 // app.use
 
+// import routes
+const momentsRoute = require('./routes/moments');
+app.use('/moments', momentsRoute);
+
+const userRoute = require('./routes/user');
+app.use('/user', userRoute);
 
 // ROUTES here
 app.get('/', (req,res) => {
     res.send('This is the home page');
 });
 
-app.get('/personality-test', (req,res) => {
-    res.send('This is the personality test page?');
-});
 
 //app.post for submitting forms??
 
@@ -21,6 +25,6 @@ app.get('/personality-test', (req,res) => {
 app.listen(3000);
 
 //Connect to database
-mongoose.connect('mongodb+srv://j6kao:vincentwan4@cluster0.6suns.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', () => 
-    console.log('connected to databse????')
+mongoose.connect(process.env.DB_CONNECT, () => 
+    console.log('connected to database!')
 );
