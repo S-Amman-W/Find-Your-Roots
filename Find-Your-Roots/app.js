874e0bf3ca0011278887a751c5cf43396e68330a
kaojonathan <kaojonathan1@gmail.com>
2021-09-18 11:15:20 -0400

    fixing app.js

--- Find-Your-Roots/app.js
+++ Find-Your-Roots/app.js
@@ -3,19 +3,17 @@ const express = require('express');
 const app = express();
 
 // Middlewares  - functions that run when we hit a route
-app.use('/personality-test', () => {
-    console.log('This is a middleware function for personality test?');
-})
+// app.use
 
 
 // ROUTES here
 app.get('/', (req,res) => {
-    res.send('This is the home page')
-})
+    res.send('This is the home page');
+});
 
 app.get('/personality-test', (req,res) => {
-    res.send('This is the personality test page?')
-})
+    res.send('This is the personality test page?');
+});
 
 //app.post for submitting forms??
 
