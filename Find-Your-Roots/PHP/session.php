186a1c526fe74ec9ea82dc935265e1d8277779e2
Rachel Butler <rachel.butler.ca@gmail.com>
2021-09-18 16:45:39 -0400

    created session.php

---
+++ Find-Your-Roots/PHP/session.php
@@ -0,0 +1,13 @@
+<?php
+// start session
+session_start();
+
+// if user already logged in -> redirect to welcome page
+if (isset($_SESSION["userid"]) && $_SESSION["userid"] === true){
+    header("location: welcome.php");
+    exit;
+}
+
+/* received help from tutorial: https://dzone.com/articles/ceate-a-login-system-using-html-php-and-mysql */
+
+?>
